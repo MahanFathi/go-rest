184f34410ba8373b29abe5f0dd1294116880f65e
Mahan Fathi <mahan0fathi@gmail.com>
2020-11-21 20:13:37 +0330

    Add simple post api + Add simple get by id api

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
-	"strconv"
+	"strings"
+	"sync"
+	"time"
 )
 
 type Coaster struct {
@@ -15,6 +19,7 @@ type Coaster struct {
 }
 
 type CoasterHandlers struct {
+	sync.Mutex
 	store map[string]Coaster
 }
 
@@ -23,11 +28,11 @@ func newCoasterHandlers() *CoasterHandlers {
 	h := CoasterHandlers{
 		store: make(map[string]Coaster),
 	}
-	for i:=0; i < 10; i++ {
-		h.store[strconv.Itoa(i)] = Coaster {
-			Name: strconv.Itoa(i),
-		}
-	}
+	// for i:=0; i < 10; i++ {
+	// 	h.store[strconv.Itoa(i)] = Coaster {
+	// 		Name: strconv.Itoa(i),
+	// 	}
+	// }
 	return &h
 }
 
@@ -51,28 +56,80 @@ func (h *CoasterHandlers) coasters(w http.ResponseWriter, r *http.Request) {
 func (h *CoasterHandlers) get(w http.ResponseWriter, r *http.Request) {
 	coasters := make([]Coaster, len(h.store))
 	i := 0
+	h.Lock()
 	for _, coaster := range h.store {
 		coasters[i] = coaster
 		i++
 	}
+	h.Unlock()
 	jsonBytes, err := json.Marshal(coasters)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		w.Write([]byte(err.Error()))
 	}
 	w.Header().Add("content-type", "application/json")
-	w.WriteHeader(http.StatusOK)
 	w.Write(jsonBytes)
+	w.WriteHeader(http.StatusOK)
 }
 
 
 func (h *CoasterHandlers) post(w http.ResponseWriter, r *http.Request) {
+	bodyBytes, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	}
+
+	ct := r.Header.Get("content-type")
+	if ct != "application/json" {
+		w.WriteHeader(http.StatusUnsupportedMediaType)
+		w.Write([]byte("content is not json type"))
+		return
+	}
+
+	defaultID := fmt.Sprintf("%d", time.Now().UnixNano())
+	var coaster Coaster
+	coaster.ID = defaultID
+	err = json.Unmarshal(bodyBytes, &coaster)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+	}
+	h.store[coaster.ID] = coaster
+	h.Lock()
+	defer h.Unlock()
+}
+
+
+func (h *CoasterHandlers) getCoaster(w http.ResponseWriter, r *http.Request) {
+	parts := strings.Split(r.URL.String(), "/")
+	if len(parts) != 3 {
+		w.WriteHeader(http.StatusNotFound)
+	}
+	h.Lock()
+	coaster, ok := h.store[parts[2]]
+	h.Unlock()
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(fmt.Sprintf("Coaster with ID %v not found", parts[2])))
+		return
+	}
+	jsonBytes, err := json.Marshal(coaster)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	}
+	w.Header().Add("content-type", "application/json")
+	w.Write(jsonBytes)
+	w.WriteHeader(http.StatusOK)
 }
 
 
 func main() {
 	coasterHandlers := newCoasterHandlers()
 	http.HandleFunc("/coasters", coasterHandlers.coasters)
+	http.HandleFunc("/coasters/", coasterHandlers.getCoaster)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
